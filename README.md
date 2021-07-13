1ea0b3c1f51b41caea37e12c5781ab841537574f
Joaquín Villanueva <villanuevajoaquin94@gmail.com>
2021-07-13 20:34:57 -0300

    readme.md updated. added some solutions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Code Wars JavaScript solved challenges
+# Code Wars JavaScript solved challenges 🚀
+
+> This is a resource for anyone wanting to find a solution to JavaScript challenges on codewars.com. I encourage everyone to commit your own solutions in the language of your preference!
+
+### 🔍 You can find me on [Twitter](https://twitter.com/_JoaquinScript) and on [LinkedIn](https://www.linkedin.com/in/joaquinvillanueva/).
---
+++ index.js
@@ -0,0 +1,70 @@
+//! 8kyu
+
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Example:
+
+// makeNegative(1); // return -1
+// makeNegative(-5); // return -5
+// makeNegative(0); // return 0
+// makeNegative(0.12); // return -0.12
+// Notes:
+
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+function makeNegative(num) {
+  if (num < 0) {
+    return num * 1;
+  }
+  if (num > 0) {
+    return num * -1;
+  }
+  if (num === 0) {
+    return 0;
+  }
+}
+
+//! 7kyu
+
+// The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+// Input
+// Input will consist of a list of lists containing two items each. Each list contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+// Note for F#: The input will be of (int list list) which is a List<List>
+
+// Example Input
+// [[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]
+// Output
+// Output will consist of a list of string values (in Haskell: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+// Example Output
+// ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+function openOrSenior(data) {
+  //destructure the elements in the array passed as a param and compare their values with the ones stated in the above
+  return data.map(([age, handicap]) =>
+    age > 54 && handicap > 7 ? "Senior" : "Open"
+  );
+}
