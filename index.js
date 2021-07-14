84a1804846162440b258b4ddd02431895763a22c
Joaquín Villanueva <villanuevajoaquin94@gmail.com>
2021-07-13 21:14:18 -0300

    exercises added

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 //! 8kyu
 
-// Nathan loves cycling.
-
-// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
-
-// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
-
+// Nathan loves cycling. Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling. You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
 // For example:
-
 // time = 3 ----> litres = 1
-
 // time = 6.7---> litres = 3
-
 // time = 11.8--> litres = 5
 
 function litres(time) {
@@ -19,17 +11,12 @@ function litres(time) {
 }
 
 // In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
-
 // Example:
-
 // makeNegative(1); // return -1
 // makeNegative(-5); // return -5
 // makeNegative(0); // return 0
 // makeNegative(0.12); // return -0.12
-// Notes:
-
-// The number can be negative already, in which case no change is required.
-// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+// Notes: The number can be negative already, in which case no change is required. Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
 
 function makeNegative(num) {
   if (num < 0) {
@@ -43,27 +30,102 @@ function makeNegative(num) {
   }
 }
 
+// Your task is to create a function that does four basic mathematical operations.
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+// Examples
+// basicOp('+', 4, 7)         // Output: 11
+// basicOp('-', 15, 18)       // Output: -3
+// basicOp('*', 5, 5)         // Output: 25
+// basicOp('/', 49, 7)        // Output: 7
+
+function basicOp(operation, value1, value2) {
+  return eval(value1 + operation + value2);
+}
+
+// Very simple, given a number, find its opposite.
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+  return number * -1;
+}
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+function squareSum(numbers) {
+  // "accumulator"  is a variable that is what was returned from the function on the previous run. "currentValue" is the element in the array being currently iterated
+  return numbers.reduce(
+    (accumulator, currentValue) => accumulator + currentValue * currentValue,
+    0 //initial value
+  );
+}
+
+// A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+// Return True if yes, False otherwise :)
+
+function hero(bullets, dragons) {
+  if (bullets === dragons * 2 || bullets >= dragons * 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+// Example:
+// 348597 => [7,9,5,8,4,3]
+
+function digitize(n) {
+  //check if n is positive
+  if (n > 0) {
+    //turn param into string
+    const toStr = String(n);
+    //make an array which elements are each character in the string passed as a param
+    const splitStr = toStr.split("");
+    //create a new array which elements are the elements in the previous array and turn them into a number
+    const mappedSplitStr = splitStr.map(Number);
+    //reverse the order of the elements in the array
+    return mappedSplitStr.reverse();
+  }
+  //if n is not positive, don't return anything
+  return;
+}
+
+// The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+// For example:
+// 1.08 --> 30
+// Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+
+function cockroachSpeed(s) {
+  //calculate how many seconds there are in an hour
+  const secsInHour = 3600;
+  //calculate how many cm there are in a km
+  const centimetersInKilometers = 100000;
+  //convert the speed to cm/s and make it an integer
+  return Math.floor((s * centimetersInKilometers) / secsInHour);
+}
+
 //! 7kyu
 
 // The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
-
 // To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
-
 // Input
 // Input will consist of a list of lists containing two items each. Each list contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
-
 // Note for F#: The input will be of (int list list) which is a List<List>
-
 // Example Input
 // [[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]
 // Output
 // Output will consist of a list of string values (in Haskell: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
-
 // Example Output
 // ["Open", "Open", "Senior", "Open", "Open", "Senior"]
 
 function openOrSenior(data) {
-  //destructure the elements in the array passed as a param and compare their values with the ones stated in the above
+  //destructure the elements in the array passed as a param and compare their values with the ones stated above
   return data.map(([age, handicap]) =>
     age > 54 && handicap > 7 ? "Senior" : "Open"
   );
